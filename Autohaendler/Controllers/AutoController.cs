035fd48da29e0148230f0390c8c5746f566003d6
Olivier_Luethy <olivier@kauz.ch>
2022-05-06 10:26:55 +0200

    Projektdateien hinzufügen.

---
+++ Autohaendler/Controllers/AutoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Autohaendler.Controllers
+{
+    public class AutoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
